6078cd62a2c72f150e0c278f9843955442b420cc
dar1o <dario.estupinan@gmail.com>
2019-04-23 08:44:23 -0500

    adding the check for the search in google

--- cypress/integration/SearchEngines/GoogleSearch/searchKeywordFeature.js
+++ cypress/integration/SearchEngines/GoogleSearch/searchKeywordFeature.js
@@ -8,6 +8,6 @@ Given(`I look for a specific keyword in google search engine`, () => {
 
 // This is the same step that we have in socialNetworks/Facebook/different.js, but you don't have to worry about collisions!
 Then(`I see the results of my search on the engine`, () => {
-  cy.log(`cy tytle contains ${JSON.stringify(cy.title())}`);
-  cy.title().should("include", "cypress-cucumber-example");
+  cy.get("head > title").contains("cypress cucumber example");
+  //cy.title().should("include", "cypress-cucumber-example");
 });
